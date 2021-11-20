e91b8d42b01154b818a12c1a8eb47731f7fd36ac
Oleg <30oleg.zv94@gmail.com>
2021-11-20 18:02:15 +0300

    added adaptive for slider

--- js/skript.js
+++ js/skript.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-  $('.slider').slick();
+  $('.slider').slick({
+    dots: true,
+    adaptiveHeight: true,
+    speed: 700,
+    fade: true,
+    cssEase: 'ease-in',
+    waitForAnimate: false,
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          autoplay:true,
+          autoplaySpeed: 1500,
+          infinite: true,
+        }
+      }
+    ]
+  });
 });
